84bd790afbe74ca9e83d44b040a7b3e5db558bdb
pengshifu <pengshifu@panda.tv>
2022-01-06 21:00:15 +0800

    Channel out bound invoker

--- transport/src/main/java/io/netty/channel/ChannelInboundInvoker.java
+++ transport/src/main/java/io/netty/channel/ChannelInboundInvoker.java
@@ -19,15 +19,18 @@ public interface ChannelInboundInvoker {
 
     /**
      * A {@link Channel} was registered to its {@link EventLoop}.
+     * 创建一个Channel 注册到EventLoop
      *
      * This will result in having the  {@link ChannelInboundHandler#channelRegistered(ChannelHandlerContext)} method
      * called of the next  {@link ChannelInboundHandler} contained in the  {@link ChannelPipeline} of the
      * {@link Channel}.
+     *
      */
     ChannelInboundInvoker fireChannelRegistered();
 
     /**
      * A {@link Channel} was unregistered from its {@link EventLoop}.
+     * 此Channel从EventLoop取消注册
      *
      * This will result in having the  {@link ChannelInboundHandler#channelUnregistered(ChannelHandlerContext)} method
      * called of the next  {@link ChannelInboundHandler} contained in the  {@link ChannelPipeline} of the
@@ -46,6 +49,7 @@ public interface ChannelInboundInvoker {
 
     /**
      * A {@link Channel} is inactive now, which means it is closed.
+     * 这个Channel是非活跃的，意味着他已关闭
      *
      * This will result in having the  {@link ChannelInboundHandler#channelInactive(ChannelHandlerContext)} method
      * called of the next  {@link ChannelInboundHandler} contained in the  {@link ChannelPipeline} of the
@@ -55,6 +59,7 @@ public interface ChannelInboundInvoker {
 
     /**
      * A {@link Channel} received an {@link Throwable} in one of its inbound operations.
+     * 这个Channel在其入站操作之一收到了 Throwable
      *
      * This will result in having the  {@link ChannelInboundHandler#exceptionCaught(ChannelHandlerContext, Throwable)}
      * method  called of the next  {@link ChannelInboundHandler} contained in the  {@link ChannelPipeline} of the
@@ -64,6 +69,7 @@ public interface ChannelInboundInvoker {
 
     /**
      * A {@link Channel} received an user defined event.
+     * Channel收到用户定义事件
      *
      * This will result in having the  {@link ChannelInboundHandler#userEventTriggered(ChannelHandlerContext, Object)}
      * method  called of the next  {@link ChannelInboundHandler} contained in the  {@link ChannelPipeline} of the
@@ -73,6 +79,7 @@ public interface ChannelInboundInvoker {
 
     /**
      * A {@link Channel} received a message.
+     * Channel 收到一条信息
      *
      * This will result in having the {@link ChannelInboundHandler#channelRead(ChannelHandlerContext, Object)}
      * method  called of the next {@link ChannelInboundHandler} contained in the  {@link ChannelPipeline} of the
@@ -83,6 +90,7 @@ public interface ChannelInboundInvoker {
     /**
      * Triggers an {@link ChannelInboundHandler#channelReadComplete(ChannelHandlerContext)}
      * event to the next {@link ChannelInboundHandler} in the {@link ChannelPipeline}.
+     * 触发一个channelReadComplete事件去ChannelPipeline中的下一个ChannelInboundHandler
      */
     ChannelInboundInvoker fireChannelReadComplete();
 
